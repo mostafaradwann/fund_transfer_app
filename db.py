2428d198369baf4eaac7a3d4034c209a7f4fbb44
mostafaradwann <mostafaredone@gmail.com>
2023-10-27 09:37:51 +0200

    DB Update & import/show all

--- db.py
+++ db.py
@@ -1,3 +1,20 @@
+#import sqlite module
 import sqlite3
+# create database and Connect
 
-conn = sqlite3.connect("accounts.sqlite")
+db = sqlite3.connect("accounts.db")
+# setting up cursor
+cr = db.cursor()
+#create table 
+sql_query = """CREATE TABLE if not exists accounts (
+    id text PRIMARY KEY,
+    name text NOT NULL, 
+    balance float NOT NULL
+    )"""
+cr.execute(sql_query)
+
+#Commit changes
+db.commit()
+
+#Close Database
+db.close
--- main.py
+++ main.py
@@ -3,54 +3,108 @@
 # List All Account , /accounts
 # GET an account inforamtion, /account<id>
 # transfer funds, at /transfer
-
-
 from flask import Flask, jsonify, request, render_template
+import sqlite3
+import csv
+import codecs
+import pandas as pd
 
 app = Flask(__name__)
 
-accounts_list = [
-    {
-        "id" : "cc26b56c-36f6-41f1-b689-d1d5065b95af",
-        "name" : "Joy Dean",
-        "balance" :4497.22,
-    },
-    {
-        "id" : "be6acfdc-cae1-4611-b3b2-dfb5167ba5fe",
-        "name" : "Bryan Rice",
-        "balance" :2632.76,
-    },{
-        "id" : "43caa0b8-76a4-4e61-b7c3-f2f5ee4b4f77",
-        "name" : "Ms. Jamie Lopez",
-        "balance" :1827.85,
-    },{
-        "id" : "69c93967-e20f-4735-9b8d-1b7dd56340ab",
-        "name" : "Lauren David",
-        "balance" :9778.7,
-    },
-]
+#database connection function
+def db_connection():
+    db = None
+    try:
+        db = sqlite3.connect("accounts.db")
+        cr = db.cursor()
+
+    except sqlite3.error as e:
+        print(e)
+    return db, cr
 
 @app.route('/')
 def homepage():
     return render_template("home.html", pagetitle="Home")
 
-# @app.route('/accounts/import', methods=['POST'])
-# def import_accounts():
-#     pass
+db, cr = db_connection()
+@app.route('/accounts/import', methods=['POST'])
+def import_accounts():
+    db, cr = db_connection()
+    
+    if 'file' not in request.files:
+        return "no file in request"
+    
+    file = request.files['file']
+
+    if file.filename == '':
+        return " no Selected file"
+    
+    #variable to store accounts data
+    accounts = []
+    if file:
+        filename = file.filename
+        if filename.endswith('.csv'):
+            
+            csv_reader = csv.DictReader(codecs.iterdecode(file, 'utf-8'), delimiter=",")
+
+            #add data to the accounts list
+            for row in csv_reader:
+                accounts.append({
+                    "id" : row["ID"],
+                    "name" : row["Name"],
+                    "balance" : row["Balance"]
+                })
+
+        elif filename.lower().endswith(('.xls', '.xlsx', '.xlsm', '.xlsb', '.odf', '.ods', '.odt')):
+            df = pd.read_excel(file)
+            df.columns = map(str.lower, df.columns)  
+            data = df.to_dict(orient='records')
+            accounts.extend(data)
+            #add data to the accounts list
+
+        sql_query = "INSERT INTO accounts ('id', 'name', 'balance') values(?,?,?)"
+        try:
+            # Begin a transaction
+            db.execute('BEGIN TRANSACTION')
+
+            for account in accounts:
+                cr.execute(sql_query, (account['id'], account['name'], account['balance']))
+
+            # Commit the transaction
+            db.commit()
+        
+        except sqlite3.Error as e:
+            db.rollback()  # Roll back the transaction in case of an error
+            print(e)
+
+        finally:
+            db.close()  # Always close the database connection
+        return jsonify(accounts)
+            
+
 
 @app.route("/accounts", methods=["GET"])
 def get_all_accounts():
+    db, cr = db_connection()
     if request.method == "GET":
-        if len(accounts_list) > 0:
-            return jsonify(accounts_list)
+        cr.execute("SELECT * From accounts")
+        rows = cr.fetchall()
+        accounts = [
+            dict(id = row[0], name = row[1], balance = row[2])
+            for row in rows
+        ]
+        db.close()
+        if accounts is not None:
+            return jsonify(accounts)
+    
 
 
-@app.route("/account/<id>", methods=["GET"])
-def get_account_by_id(id):
-    if request.method == "GET":
-        for account in accounts_list:
-            if account['id'] == id:
-                return jsonify(account)
+# @app.route("/account/<id>", methods=["GET"])
+# def get_account_by_id(id):
+#     if request.method == "GET":
+#         for account in accounts_list:
+#             if account['id'] == id:
+#                 return jsonify(account)
 
 # @app.route("/transfer", methods=["POST"])
 # def transfer_fund():
